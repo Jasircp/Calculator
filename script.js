530922f51795f24ee9346a730a0b7b6eb3d3ef6e
jasircp26@gmail.com <jasircp26@gmail.com>
2023-05-30 20:15:47 +0530

    second

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var a="",result,flag=1;
+var a="",result,flag=1,mode=1;
 function clr(){
     document.getElementById("screen").value=""
 }
@@ -6,27 +6,38 @@ function clk(num){
     document.getElementById("screen").value+=num
     flag=0
 }
-/*function op(oper){
-    if(flag==0){
-        a+=document.getElementById("screen").value
-        switch(oper){
-            case '+':a+="+"
-            break
-            case '-':a+="-"
-            break
-            case '*':a+="*"
-            break
-            case '/':a+="/"
-            break
-        }
-        document.getElementById("screen").value=""
-        flag=1;
+function modeChange(){
+    if(mode==1){
+        mode=2;
     }
-}*/
+    else{
+        mode=1
+    }
+    console.log(mode);
+}
 function op(oper){
-    if(flag==0){
-        document.getElementById("screen").value+=oper
-        flag=1
+    if(mode==1){
+        if(flag==0){
+            a+=document.getElementById("screen").value
+            switch(oper){
+                case '+':a+="+"
+                break
+                case '-':a+="-"
+                break
+                case '*':a+="*"
+                break
+                case '/':a+="/"
+                break
+            }
+            document.getElementById("screen").value=""
+            flag=1;
+        }
+    }
+    else{
+        if(flag==0){
+            document.getElementById("screen").value+=oper
+            flag=1
+        }
     }
 }
 function ans(){
@@ -34,4 +45,12 @@ function ans(){
     result = eval(a)
     document.getElementById("screen").value=result
     a=""
+}
+function toggleTheme(){
+          var btn = document.getElementById("myButton");
+          if (btn.innerHTML == "Classic") {
+            btn.innerHTML = "Modern";
+          } else {
+            btn.innerHTML = "Classic";
+          }
 }
